3dbdb31e3c49be77819c0d2028e26d1d5b374e76
luismacd <20220454@aloe.ulima.edu.pe>
2025-11-30 19:14:39 -0500

    conexion bd railway

--- PROYECTO2.0/backend/src/config/database.js
+++ PROYECTO2.0/backend/src/config/database.js
@@ -6,7 +6,7 @@ dotenv.config();
 // Usar variable de entorno `DATABASE_URL` si está disponible
 const CADENA_CONEXION =
   process.env.DATABASE_URL ||
-  "postgres://postgres:asuna%403101@localhost:5432/postgres";
+  "postgres://postgres:1234@localhost:5432/postgres";
 
 const sequelize = new Sequelize(CADENA_CONEXION, {
   logging: false, // desactiva logs de SQL en consola
